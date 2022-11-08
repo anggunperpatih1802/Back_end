28bcd5deb573d3901665d54ea5b09df0917602ce
aperpatih <anggun.perpatih@biz.linknet.co.id>
2022-11-08 10:21:25 +0700

    commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class MainClass
+{
+
+    public static string SearchingChallenge(string str)
+    {
+
+        int stringLength = str.Length;
+        int maxPalindromeStringLength = 0;
+        int maxPalindromeStringStartIndex = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            int currentCharIndex = i;
+
+            for (int lastCharIndex = stringLength - 1; lastCharIndex > currentCharIndex; lastCharIndex--)
+            {
+                bool isPalindrome = true;
+
+                if (str[currentCharIndex] != str[lastCharIndex])
+                {
+                    continue;
+                }
+
+                for (int nextCharIndex = currentCharIndex + 1; nextCharIndex < lastCharIndex / 2; nextCharIndex++)
+                {
+                    if (str[nextCharIndex] != str[lastCharIndex - 1])
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                if (isPalindrome)
+                {
+                    if (lastCharIndex + 1 - currentCharIndex > maxPalindromeStringLength)
+                    {
+                        maxPalindromeStringStartIndex = currentCharIndex;
+                        maxPalindromeStringLength = lastCharIndex + 1 - currentCharIndex;
+                    }
+                }
+                break;
+            }
+        }
+
+        return str.Substring(maxPalindromeStringStartIndex, maxPalindromeStringLength);
+        //return str;
+
+    }
+
+    static void Main()
+    {
+        // keep this function call here
+        Console.WriteLine(SearchingChallenge("hellosannasmith"));
+    }
+
+}
